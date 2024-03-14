1e77d6f0caaacb48e48f72bdfc85d43811166477
SaaW008 <saad.achnani@gmail.com>
2024-03-13 18:28:48 -0700

    Update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 
 index = 0
 for lettre in range(ord('z'), ord('a') - 1, -1):
-        print("{}".format(chr(lettre - index)), end="")
-            index = 32 if index == 0 else 0
+    print("{}".format(chr(lettre - index)), end="")
+    index = 32 if index == 0 else 0
